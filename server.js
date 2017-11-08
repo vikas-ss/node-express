7e16461897e9e7e3b52fe198fa21c7a340515911
vikas-ss <vikas.singla86@gmail.com>
2017-11-08 20:42:21 +0530

    Changed port and start script

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const s = require("express");
 const hbs = require("hbs");
 
+const port = process.env.port || 3000;
 var app = s();
 
 app.set("view engine", "hbs");
@@ -15,6 +16,22 @@ app.get("/", (req, res) => {
 
 });
 
+
+app.get("/todos", (req, res) => {
+
+  res.send({
+    title: "Go to dentist",
+    completed: false
+  });
+});
+
+
+app.post("/todos", (req, res) => {
+
+  //console.log(req);
+  res.send("Saved to database");
+});
+
 app.get("/about", (req, res) => {
 
   //res.send("About page ... ");
@@ -23,7 +40,6 @@ app.get("/about", (req, res) => {
     age: 31,
     address: "17042 Aggarwal Colony Bathinda"
   });
-
 });
 
-app.listen(3000);
+app.listen(port);
