570b77f5ef3dd2f4c682d10b7b701bee5655235c
vikas-ss <vikas.singla86@gmail.com>
2017-11-08 21:02:55 +0530

    port changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const s = require("express");
 const hbs = require("hbs");
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 var app = s();
 
 app.set("view engine", "hbs");
